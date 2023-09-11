c65296f5dffce359369810abb45ef7d0c48d2e45
MNlegion <rkreuser30@gmail.com>
2023-09-10 21:54:06 -0400

    continued user input logic via inquirer

--- app.js
+++ app.js
@@ -9,30 +9,103 @@ import inquirer from "inquirer";
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+          },
+          {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+          },
+          {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+          }
+    ]);
+    // .then((answers) => {
+    //   console.log(answers);
+    //   // Use user feedback for... whatever!!
+    // })
+    // .catch((error) => {
+    //   if (error.isTtyError) {
+    //     // Prompt couldn't be rendered in the current environment
+    //   } else {
+    //     // Something else went wrong
+    //   }
+    // });
+};
 
-inquirer
-  .prompt([
-    {
+const promptProject = portfolioData => {
+    
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer.prompt([
+      {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
-    }, 
-    {
+        message: 'What is the name of your project?'
+      },
+      {
         type: 'input',
-        name: 'email',
-        message: 'What is your email?'
-    }
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    console.log(answers);
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+  };
 
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+    // will be uncommented
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
